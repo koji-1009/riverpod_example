4510a8877665703360dec55e8b04f916b340343a
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-08-01 02:15:04 +0900

    Add theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_example/home.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,18 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: FlexColorScheme.light(
+        scheme: FlexScheme.indigo,
+        appBarElevation: 4,
+      ).toTheme,
+      darkTheme: FlexColorScheme.dark(
+        scheme: FlexScheme.indigo,
+        appBarElevation: 4,
+      ).toTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
