627e23dd4fe4e7fad0efc12d2f02a3d5934dfad2
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-08-01 02:36:39 +0900

    Add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_example/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
